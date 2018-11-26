1ebb3e623a86baa499bec04c450c9b00276ee208
renjiraND <naufhalrenjira@gmail.com>
2018-11-27 01:06:10 +0700

    Added buyBookByID function and merged bookservice with master

--- webservice/BookService/src/bookservice/BookService.java
+++ webservice/BookService/src/bookservice/BookService.java
@@ -12,11 +12,14 @@ import javax.xml.ws.Endpoint;
 
 import java.net.*;
 import java.io.*;
+import java.text.*;
+import java.util.*;
 
 @WebService(targetNamespace = "http://test")
 public class BookService {
 
-  public String APIkey = "AIzaSyCozd0JbgrBxT32kZILK2gKfh7wSKKVOf4";
+  private String APIkey = "AIzaSyCozd0JbgrBxT32kZILK2gKfh7wSKKVOf4";
+  private String BankID = "000011112222";
 
   public static void main(String[] argv) {
     Object implementor = new BookService ();
@@ -28,8 +31,40 @@ public class BookService {
   public String searchBook(String keyword) throws IOException,ParseException {
     System.out.println("1");
     URL url = new URL("https://www.googleapis.com/books/v1/volumes?q="+keyword+"&key="+APIkey);
+    StringBuffer content = connectHttpUrl(url);
+
+    String json_string;
+
+    JSONParser jsonParse = new JSONParser();
+    json_string = content.toString();
+
+    JSONObject JSONBooks = (JSONObject) jsonParse.parse(json_string);
+
+
+//    for (int i = 0; i < ; i++) {
+//      JSONObject object = (JSONObject) JSONBooks.get(i);
+//      System.out.println(object);
+//    }
+    System.out.println(JSONBooks.get("totalItems"));
+    return json_string;
+  }
+
+
+  public void buyBookByID(String BookID, String UserID) throws IOException{
+    // localhost:4000/transfer?send=123412341234&rcv=040214100804&amount=0&time=2018-11-15%2000:00:00
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd%20HH:mm:ss");
+    Date date = new Date();
+    String varTime =  dateFormatter.format(date);
+    String varAmount = "0"; // testing purposes
+
+    URL url = new URL("http://localhost:4000/transfer?send="+UserID+"&rcv="+BankID+"&amount="+varAmount+"&time="+varTime);
+    StringBuffer received = connectHttpUrl(url);
+    System.out.println(received);
+
+  }
+
+  private StringBuffer connectHttpUrl(URL url) throws IOException{
     HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    System.out.println("2");
     con.setRequestMethod("GET");
     con.setRequestProperty("Content-Type", "application/json");
     con.setConnectTimeout(5000);
@@ -47,20 +82,6 @@ public class BookService {
     in.close();
     System.out.println(content);
     con.disconnect();
-
-    String JSONstring;
-
-    JSONParser jsonParse = new JSONParser();
-    JSONstring = content.toString();
-
-    JSONObject JSONBooks = (JSONObject) jsonParse.parse(JSONstring);
-
-
-//    for (int i = 0; i < ; i++) {
-//      JSONObject object = (JSONObject) JSONBooks.get(i);
-//      System.out.println(object);
-//    }
-    System.out.println(JSONBooks.get("totalItems"));
-    return JSONstring;
+    return content;
   }
 }
