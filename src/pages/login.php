6ab7570050ef7f7ad35d31c976f88b553217c2f9
NPP0804 <ajienaufal0804@gmail.com>
2018-11-28 07:14:29 +0700

    Token done

--- src/pages/login.php
+++ src/pages/login.php
@@ -54,11 +54,33 @@
       return false;
     }
   }
+  function get_client_ip_server() {
+    $ipaddress = '';
+    if ($_SERVER['HTTP_CLIENT_IP'])
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if($_SERVER['HTTP_X_FORWARDED'])
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if($_SERVER['HTTP_FORWARDED_FOR'])
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if($_SERVER['HTTP_FORWARDED'])
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if($_SERVER['REMOTE_ADDR'])
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+
+    return $ipaddress;
+  }
   function addTokenToDB($token,$user)
   {
     require '../php/connect.php';
-    $exptime = date('Y-m-d H:i:s',(time()+43200));
-    $sql = "INSERT INTO probook.token(access_token, granted, expiry_time) VALUES ('$token','$user','$exptime')";
+    date_default_timezone_set("Asia/Jakarta");
+    $exptime = date('Y-m-d H:i:s',(time()+3600));
+    $browser = $_SERVER['HTTP_USER_AGENT'];
+    $ip = get_client_ip_server();
+    $sql = "INSERT INTO probook.token(access_token, granted, expiry_time, browser, ip) VALUES ('$token','$user','$exptime', '$browser', '$ip')";
     if ($conn->query($sql) === TRUE) {
     } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
